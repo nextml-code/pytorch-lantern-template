86268ba181abc239614811a51d23c1a32bfc72f7
Richard <samedii@gmail.com>
2020-11-21 18:56:46 +0100

    test: remove pip cache

--- test/run.sh
+++ test/run.sh
@@ -10,4 +10,4 @@ cd "$PROJECT_PATH"
 poetry install
 poetry add ../../test/wildfire-0.0.0-py3-none-any.whl
 poetry run guild run prepare -y
-poetry run guild run train max_epochs=2 -y
+poetry run guild run train max_epochs=2 n_batches_per_epoch=5 -y

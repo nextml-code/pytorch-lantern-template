14b0caeedaad707068af1225ae65af8535719390
Richard <samedii@gmail.com>
2020-11-25 18:51:08 +0100

    improve: evaluate operation and move train

--- test/run.sh
+++ test/run.sh
@@ -10,3 +10,4 @@ cd "$PROJECT_PATH"
 poetry run guild run prepare -y
 poetry run guild run train max_epochs=2 n_batches_per_epoch=5 -y
 poetry run guild run retrain max_epochs=2 n_batches_per_epoch=5 -y
+poetry run guild run evaluate -y

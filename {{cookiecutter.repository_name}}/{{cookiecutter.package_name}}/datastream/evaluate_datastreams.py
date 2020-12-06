879c6cdcd1d82fe913d9e7b866341b5de2837689
Richard <samedii@gmail.com>
2020-12-06 10:28:30 +0100

    refactor: less verbose evaluate datastream

--- {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/datastream/evaluate_datastreams.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/datastream/evaluate_datastreams.py
@@ -4,12 +4,7 @@ from {{cookiecutter.package_name}} import datastream
 
 
 def evaluate_datastreams():
-    datasets = datastream.datasets()
     return {
         split_name: Datastream(dataset)
-        for split_name, dataset in dict(
-            gradient=datasets["gradient"],
-            early_stopping=datasets["early_stopping"],
-            compare=datasets["compare"],
-        ).items()
+        for split_name, dataset in datastream.datasets().items()
     }

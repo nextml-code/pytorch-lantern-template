7f7c2a1a0fde30a02af074b6e87522cff69b81f8
Richard <samedii@gmail.com>
2020-12-17 10:03:55 +0100

    improve: new arguments to cookiecutter and remove private repo

--- test/env.sh
+++ test/env.sh
@@ -8,15 +8,8 @@ readonly TEST_DIR='.test-template'
 
 # Scaffold the project:
 readonly REPOSITORY_NAME='mnist-template'
-readonly PACKAGE_NAME='mnist_template'
-readonly PACKAGE_VERSION='0.0.1'
-readonly PACKAGE_DESCRIPTION='Placeholder description'
 
 readonly PROJECT_PATH="$TEST_DIR/$REPOSITORY_NAME"
 
 # Exporting variables:
 export REPOSITORY_NAME
-export PACKAGE_NAME
-export PACKAGE_VERSION
-export PACKAGE_DESCRIPTION
-export PROJECT_PATH

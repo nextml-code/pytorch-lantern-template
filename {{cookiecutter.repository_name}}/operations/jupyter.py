25a3cd6b2c312f1f585fbe245cbfbb3690a51384
FelixAbrahamsson <felixab@kth.se>
2021-02-06 10:02:59 +0100

    feature: jupyter guild op

---
+++ {{cookiecutter.repository_name}}/operations/jupyter.py
@@ -0,0 +1,28 @@
+import argparse
+import subprocess
+import os
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8888)
+    args = parser.parse_args()
+
+    working_dir = f"{os.environ['GUILD_HOME']}/runs/{os.environ['RUN_ID']}"
+    command = f"jupyter notebook --port={args.port}"
+
+    if "PYTHONSTARTUP" in os.environ and Path(os.environ["PYTHONSTARTUP"]).exists():
+        python_startup_script = (
+            Path(os.environ["PYTHONSTARTUP"]).open("r").read() + "\n"
+        )
+    else:
+        python_startup_script = ""
+
+    Path("change-dir.py").write_text(
+        f"{python_startup_script}import os\nos.chdir('{working_dir}')"
+    )
+
+    os.environ["PYTHONSTARTUP"] = f"{os.getcwd()}/change-dir.py"
+
+    subprocess.run(command, shell=True)

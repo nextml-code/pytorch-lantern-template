fe2fe0e97172508833dc92aa551fae6b6fd71cd3
FelixAbrahamsson <felixab@kth.se>
2021-02-17 15:48:37 +0100

    feature: cifar10 instead of mnist

--- {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/architecture/model.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/architecture/model.py
@@ -11,14 +11,14 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
         self.logits = ModuleCompose(
-            nn.Conv2d(1, 32, 3, 1),
+            nn.Conv2d(3, 32, 3, 1),
             F.relu,
             nn.Conv2d(32, 64, 3, 1),
             F.relu,
             partial(F.max_pool2d, kernel_size=2),
             nn.Dropout2d(0.25),
             partial(torch.flatten, start_dim=1),
-            nn.Linear(9216, 128),
+            nn.Linear(12544, 128),
             F.relu,
             nn.Dropout2d(0.5),
             nn.Linear(128, 10),

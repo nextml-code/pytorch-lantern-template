44d76a66d1d9fa90fe735218049b69a9779736d1
FelixAbrahamsson <felixab@kth.se>
2021-02-17 16:14:23 +0100

    feature: resize in feature and datastream

--- {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/datastream/evaluate_datastreams.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/datastream/evaluate_datastreams.py
@@ -1,10 +1,11 @@
 from datastream import Datastream
 
 from {{cookiecutter.package_name}} import datastream
+from {{cookiecutter.package_name}}.datastream import resize
 
 
 def evaluate_datastreams():
     return {
-        split_name: Datastream(dataset)
+        split_name: Datastream(dataset).map(lambda example: example.augment(resize))
         for split_name, dataset in datastream.datasets().items()
     }
--- {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/datastream/gradient_datastream.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/datastream/gradient_datastream.py
@@ -1,15 +1,20 @@
 from datastream import Datastream
 
 from {{cookiecutter.package_name}} import datastream
+from {{cookiecutter.package_name}}.datastream import resize
 from {{cookiecutter.package_name}}.problem import settings
 
 
 def GradientDatastream():
     dataset = datastream.datasets()["gradient"]
     augmenter = datastream.augmenter()
-    return Datastream.merge(
-        [
-            Datastream(dataset.subset(lambda df: df["class_name"] == class_name))
-            for class_name in settings.CLASS_NAMES
-        ]
-    ).map(lambda example: example.augment(augmenter))
+    return (
+        Datastream.merge(
+            [
+                Datastream(dataset.subset(lambda df: df["class_name"] == class_name))
+                for class_name in settings.CLASS_NAMES
+            ]
+        )
+        .map(lambda example: example.augment(resize))
+        .map(lambda example: example.augment(augmenter))
+    )

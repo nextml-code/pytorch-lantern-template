22c584096a7f159aef84ff5b9e9f41280004beec
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-03-23 14:53:07 +0100

    feature: more control over what jupyter op does

--- {{cookiecutter.repository_name}}/operations/jupyter.py
+++ {{cookiecutter.repository_name}}/operations/jupyter.py
@@ -3,20 +3,19 @@ import argparse
 import subprocess
 import webbrowser
 from pathlib import Path
-from notebook.services.contents.filemanager import FileContentsManager as FCM
+from notebook.services.contents.filemanager import FileContentsManager
 
 
-def jupyter(notebook, port, show):
-    original_path = Path("notebooks") / notebook
+def jupyter(notebook_name, port, clean, interactive):
+    original_path = Path("notebooks") / notebook_name
+    notebook_path = Path(notebook_name) if clean else original_path
+    if original_path.exists() and not notebook_path.exists():
+        shutil.copy(original_path, notebook_path)
+    if not notebook_path.exists():
+        FileContentsManager().new(path=notebook_path.as_posix())
 
-    copy_path = Path(notebook)
-    if original_path.exists():
-        shutil.copy(original_path, copy_path)
-    else:
-        FCM().new(path=copy_path.as_posix())
-
-    if show == show_options["interactive"]:
-        subprocess.run(["jupyter", "notebook", copy_path.as_posix()])
+    if interactive:
+        subprocess.run(["jupyter", "notebook", notebook_path.as_posix()])
     else:
         subprocess.run(
             [
@@ -26,18 +25,13 @@ def jupyter(notebook, port, show):
                 "notebook",
                 "--inplace",
                 "--execute",
-                copy_path.as_posix(),
+                notebook_path.as_posix(),
             ]
         )
 
-    subprocess.run(["jupyter", "nbconvert", "--to", "html", copy_path.as_posix()])
-    html_path = copy_path.parent / notebook.replace(".ipynb", ".html")
-    if show == show_options["done"]:
-        webbrowser.open(html_path)
-
-
-# enums are hopeless
-show_options = {option: option for option in ["interactive", "done", "nothing"]}
+    subprocess.run(["jupyter", "nbconvert", "--to", "html", notebook_path.as_posix()])
+    if not interactive:
+        webbrowser.open(notebook_path.with_suffix(".html").as_posix())
 
 
 if __name__ == "__main__":
@@ -45,10 +39,16 @@ if __name__ == "__main__":
     parser.add_argument("--notebook", type=str, required=True)
     parser.add_argument("--port", type=int, default=8888)
     parser.add_argument(
-        "--show",
-        default=show_options["interactive"],
-        choices=list(show_options),
-        help="What to show: an interactive notebook, an HTML export when done, or nothing.",
+        "--clean",
+        type=bool,
+        default=False,
+        help="Whether to copy notebooks and sourcecode - more inspectability, slower iteration",
     )
+    parser.add_argument("--interactive", type=bool, default=True)
     args = parser.parse_args()
-    jupyter(notebook=args.notebook, port=args.port, show=args.show)
+    jupyter(
+        notebook_name=args.notebook,
+        port=args.port,
+        clean=args.clean,
+        interactive=args.interactive,
+    )

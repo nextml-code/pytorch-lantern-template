470804210e67071458b82cf072d0cc5c417c2534
FelixAbrahamsson <felixab@kth.se>
2021-06-23 10:16:07 +0200

    feature: notebook and no-browser args for jupyter

--- {{cookiecutter.repository_name}}/operations/jupyter.py
+++ {{cookiecutter.repository_name}}/operations/jupyter.py
@@ -7,10 +7,14 @@ from pathlib import Path
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=8888)
+    parser.add_argument("--notebook", type=str, default="")
+    parser.add_argument("--no-browser", action="store_true")
     args = parser.parse_args()
 
     working_dir = f"{os.environ['GUILD_HOME']}/runs/{os.environ['RUN_ID']}"
-    command = f"jupyter notebook --port={args.port}"
+    command = f"jupyter notebook {args.notebook} --port={args.port}"
+    if args.no_browser:
+        command += " --no-browser"
     os.environ["PYTHONPATH"] += os.environ["PROJECT_DIR"] + ":"
 
     if "PYTHONSTARTUP" in os.environ and Path(os.environ["PYTHONSTARTUP"]).exists():
